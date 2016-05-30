9a6b8051a87fe14ac5536949e47ded4191079dee
gewentao <sailbear@foxmail.com>
2016-05-30 17:07:46 +0800

    superuser

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$ADMIN_USER_NAME', 'sailbear@foxmail.com', '$ADMIN_USER_PASSWORD')" | python manage.py shell
 python manage.py runserver 0.0.0.0:80
