3c3537e1fed31cf4056506aeaeb20e37f2ca392f
gewentao <sailbear@foxmail.com>
2016-05-30 13:59:41 +0800

    fix url

--- djangopj/urls.py
+++ djangopj/urls.py
@@ -13,8 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from blog.views import mainPage
+from blog.views import postPage
+from blog.views import archivePage
+from blog.views import galleryPage
+from blog.views import aboutPage
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,3 +29,7 @@ urlpatterns = [
     url(r'^about/$',aboutPage),
     url(r'^blog/post',postPage),
 ]
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+admin.autodiscover()
